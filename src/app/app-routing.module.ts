397eca5faa875a51395ff44b454781d9a2af89ee
Daniel's kiala <danielskiala@gmail.com>
2022-03-29 12:53:44 +0200

    third commit teams page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/site/home/home.component';
 import { JoueursComponent } from './components/site/joueurs/joueurs.component';
+import { ManagerComponent } from './components/site/manager/manager.component';
 
 const routes: Routes = [
   {path:"", redirectTo:"site/home", pathMatch:"full"},
   {path:"site/home", component:HomeComponent},
   {path:"site/joueurs", component:JoueursComponent},
+  {path:"site/manager", component:ManagerComponent},
 ];
 
 @NgModule({
