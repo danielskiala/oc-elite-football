2e933f883ad26b8246d7111ab8beaa45a013b682
Daniel's kiala <danielskiala@gmail.com>
2022-03-31 11:20:08 +0200

    Modified on home page and maths page

--- src/app/components/site/home/home.component.ts
+++ src/app/components/site/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-home',
@@ -12,4 +13,44 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  config_slider_player: SwiperOptions = {
+    // npm install --save ngx-useful-swiper@latest swiper
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    breakpoints: {
+      1600: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      1080: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      1366: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      500: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      }
+    },
+    loop: true,
+    speed: 4000,
+    autoplay:true,
+  };
 }
